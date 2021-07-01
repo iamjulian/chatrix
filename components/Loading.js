a5dc8ebe5eaf2e81a994c616ff46d82407b06b74
Julian Anthony <julian.anthony4@gmail.com>
2021-07-01 22:30:31 +0530

    fixed img error

--- components/Loading.js
+++ components/Loading.js
@@ -1,8 +1,10 @@
+import Image from 'next/image';
+
 function Loading() {
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
-        <img
+        <Image
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png "
           alt="loading"
           style={{ marginBottom: 10 }}
